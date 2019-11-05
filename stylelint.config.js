d261911ad18664da2fcbb0210e12112c55cc6daa
mekinus <machinusdeveloper@gmail.com>
2019-11-05 16:06:13 -0300

    stylelint update

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   "extends": "stylelint-config-recommended",
-  "rules": {
+  "rules": { "order/properties-alphabetical-order": null,
     "at-rule-empty-line-before": [ "always", {
       except: [
         "blockless-after-same-name-blockless",
